f5d2c67905c3c48a49ceea46923713360b954a64
Dan Hart <dhart94@gmail.com>
2020-01-09 12:54:18 -0600

    Initial Commit

---
+++ WhereAmI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//
+//  ContentView
+//  WhereAmI
+//
+//  Created by Dan Hart on 1/9/20.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
